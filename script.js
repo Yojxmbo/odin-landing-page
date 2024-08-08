2432e035e033cb93f5d2cc12a88d6decca10c80b
yojxmbo <yojxmbo@users.noreply.github.com>
2024-08-08 17:20:09 -0500

    enhanced button elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonOneEl = document.querySelector('.buttonOne');
+const buttonTwoEl = document.querySelector('.buttonTwo');
+
+
+buttonOneEl.addEventListener('click', () => {
+    alert('Still working on some kinks');
+})
+
+buttonTwoEl.addEventListener('click', () => {
+    alert('Still working on some kinks');
+})
